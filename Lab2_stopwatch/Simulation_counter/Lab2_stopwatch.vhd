0a5a4b6f67d7a28821df61f3b7e5484384f30af5
deaf-user <nate.walker.timp@gmail.com>
2024-09-23 17:50:16 -0600

    Completed Lab 2

--- Lab2_stopwatch/Simulation_counter/Lab2_stopwatch.vhd
+++ Lab2_stopwatch/Simulation_counter/Lab2_stopwatch.vhd
@@ -4,47 +4,150 @@ use ieee.numeric_std.all;
 
 entity Lab2_stopwatch is
 	port (
-		ADC_CLK_10 : in STD_LOGIC;
-		MAX10_CLK1_50 : in STD_LOGIC;
-		MAX10_CLK2_50 : in STD_LOGIC;
-		KEY : in std_logic_vector(1 downto 0);
-		HEX0 : out STD_LOGIC_VECTOR(7 downto 0);
-		HEX1 : out STD_LOGIC_VECTOR(7 downto 0);
-		HEX2 : out STD_LOGIC_VECTOR(7 downto 0);
-		HEX3 : out STD_LOGIC_VECTOR(7 downto 0);
-		HEX4 : out STD_LOGIC_VECTOR(7 downto 0);
-		HEX5 : out STD_LOGIC_VECTOR(7 downto 0)
+		HEX0 : out std_logic_vector(7 downto 0);
+		HEX1 : out std_logic_vector(7 downto 0);
+		HEX2 : out std_logic_vector(7 downto 0);
+		HEX3 : out std_logic_vector(7 downto 0);
+		HEX4 : out std_logic_vector(7 downto 0);
+		HEX5 : out std_logic_vector(7 downto 0);
+		ADC_CLK_10 : IN STD_LOGIC;
+		MAX10_CLK1_50 : IN STD_LOGIC;
+		MAX10_CLK2_50 : IN STD_LOGIC;
+		KEY : IN STD_LOGIC_VECTOR(1 DOWNTO 0)
 	);
-end;
+end Lab2_stopwatch;
 
-architecture behavioral of Lab2_stopwatch is
 
+architecture componentlist of Lab2_stopwatch is
+	component HEX_seven_seg_disp_6
+		port (
+			clk : in std_logic;
+			IN0 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			IN1 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			IN2 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			IN3 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			IN4 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			IN5 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			HEX0 : out std_logic_vector(7 downto 0);
+			HEX1 : out std_logic_vector(7 downto 0);
+			HEX2 : out std_logic_vector(7 downto 0);
+			HEX3 : out std_logic_vector(7 downto 0);
+			HEX4 : out std_logic_vector(7 downto 0);
+			HEX5 : out std_logic_vector(7 downto 0)
+		);
+	end component HEX_seven_seg_disp_6;
+	
 	component counter
 		generic (
 			n : integer := 4;
-			div : integer := 4
+			div : integer := 4;
+			div_min : integer := 9
 		);
-		
 		port (
 			clk : in std_logic;
-			rst_l : in std_logic;
+			rst_l : in  std_logic;
+			go : in std_logic;
 			output : out std_logic_vector((n-1) downto 0)
 		);
-	end component;
-	
-	begin
-		u0 : counter
-			generic map (
-				n => 10,
-				div => 25000000
-			)
-			port map (
-				clk => MAX10_CLK1_50,
-				rst_l => key(0),
-				output => LEDR
-			);
-	
-	
-	
-	end;
-	
+	end component counter;
+	signal IN0 : std_logic_vector(3 downto 0);
+	signal IN1 : std_logic_vector(3 downto 0);
+	signal IN2 : std_logic_vector(3 downto 0);
+	signal IN3 : std_logic_vector(3 downto 0);
+	signal IN4 : std_logic_vector(3 downto 0);
+	signal IN5 : std_logic_vector(3 downto 0);
+	signal hex4_temp : std_logic_vector(7 downto 0);
+	signal hex2_temp : std_logic_vector(7 downto 0);
+begin
+	display : HEX_seven_seg_disp_6
+		port map (
+			clk => MAX10_CLK1_50,
+			IN0 => IN0,
+			IN1 => IN1,
+			IN2 => IN2,
+			IN3 => IN3,
+			IN4 => IN4,
+			IN5 => IN5,
+			HEX0 => HEX0,
+			HEX1 => HEX1,
+			HEX2 => HEX2_temp,
+			HEX3 => HEX3,
+			HEX4 => HEX4_temp,
+			HEX5 => HEX5
+		);
+	counter_H2 : counter
+		generic map (
+			n => 4,
+			div => 5,
+			div_min => 9
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN0
+		);
+	counter_H1 : counter
+		generic map (
+			n => 4,
+			div =>50,
+			div_min => 9
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN1
+		);
+	counter_S2 : counter
+		generic map (
+			n => 4,
+			div => 500,
+			div_min => 9
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN2
+		);
+	counter_S1 : counter
+		generic map (
+			n => 4,
+			div => 5000,
+			div_min => 5
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN3
+		);
+	counter_M2 : counter
+		generic map (
+			n => 4,
+			div => 300000,
+			div_min => 9
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN4
+		);
+	counter_M1 : counter
+		generic map (
+			n => 4,
+			div => 300000,
+			div_min => 5
+		)
+		port map (
+			clk => MAX10_CLK1_50,
+			rst_l => key(0),
+			go => key(1),
+			output => IN5
+		);
+	HEX4 <= "01111111" and HEX4_temp;
+	HEX2 <= "01111111" and HEX2_temp;
+
+end componentlist;
--- Lab2_stopwatch/Simulation_counter/testCounter.vhd
+++ Lab2_stopwatch/Simulation_counter/testCounter.vhd
@@ -1,54 +1,58 @@
 library ieee;
-library work;
 
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
--- use work.counter.all;
-use work.counter;
 
 entity TB_counter is
 end TB_counter;
 
 architecture behavioral of TB_counter is
 
-	-- component my_counter
-	-- 	generic (
-	-- 		N : integer := 4
-	-- 	);
-	-- 	port (
-	-- 		clk : in std_logic;
-	-- 		rst_l : in std_logic;
-	-- 		count : out unsigned((N-1) downto 0)
-	-- 	);
-	-- end component;
+	component Lab2_stopwatch
+	 	port (
+			ADC_CLK_10 : in STD_LOGIC;
+			MAX10_CLK1_50 : in STD_LOGIC;
+			MAX10_CLK2_50 : in STD_LOGIC;
+			KEY : in std_logic_vector(1 downto 0);
+			HEX0 : out STD_LOGIC_VECTOR(7 downto 0);
+			HEX1 : out STD_LOGIC_VECTOR(7 downto 0);
+			HEX2 : out STD_LOGIC_VECTOR(7 downto 0);
+			HEX3 : out STD_LOGIC_VECTOR(7 downto 0);
+			HEX4 : out STD_LOGIC_VECTOR(7 downto 0);
+			HEX5 : out STD_LOGIC_VECTOR(7 downto 0)
+	 	);
+	 end component;
 	
 	signal clk_TB : std_logic := '0';
 	signal rst_l_TB : std_logic := '1';
-	signal count_TB : std_logic_vector(7 downto 0);
+	signal go_TB : std_logic := '1';
 	
 	constant CLK_PERIOD : time := 10 ns;
 	
+	signal hex0_temp : std_logic_vector(7 downto 0);
+	signal hex1_temp : std_logic_vector(7 downto 0);
+	signal hex2_temp : std_logic_vector(7 downto 0);
+	signal hex3_temp : std_logic_vector(7 downto 0);
+	signal hex4_temp : std_logic_vector(7 downto 0);
+	signal hex5_temp : std_logic_vector(7 downto 0);
+	
 begin
 
-	-- uut : my_counter
-	-- 	generic map(
-	-- 		N => 8
-	-- 	)
-	-- 	port map(
-	-- 		clk => clk,
-	-- 		rst_l => rst_l,
-	-- 		count => count
-	-- 	);
-	uut : entity counter
-		generic map(
-			N => 8
-		)
-		port map(
-			clk => clk_TB,
-			rst_l => rst_l_TB,
-			output => count_TB
-		);
-		
+	uut : Lab2_stopwatch
+	 	port map(
+	 		ADC_CLK_10 => clk_TB,
+			MAX10_CLK1_50 => clk_TB,
+			MAX10_CLK2_50 => clk_TB,
+			KEY(0) => rst_l_TB,
+			key(1) => go_TB,
+			HEX0 => hex0_temp,
+			HEX1 => hex1_temp,
+			HEX2 => hex2_temp,
+			HEX3 => hex3_temp,
+			HEX4 => hex4_temp,
+			HEX5 => hex5_temp
+	 	);
+
 	clk_process : process
 	begin
 		clk_TB <= '0';
@@ -59,10 +63,19 @@ begin
 	
 	stm_process : process
 	begin
-		rst_l_TB <= '1';
-		wait for clk_period * 10;
+		wait for clk_period *10;
+		go_TB <= '0';
+		wait for clk_period * 20;
+		go_TB <= '1';
+		wait for clk_period *5;
 		rst_l_TB <= '0';
-		wait for clk_period * 10;
+		wait for clk_period * 5;
+		rst_l_TB <= '1';
+		wait for clk_period * 5;
+		go_TB <= '0';
+		wait for clk_period * 200;
+		rst_l_TB <= '0';
+		wait for clk_period * 5;
 		rst_l_TB <= '1';
 		wait;
 	end process;
