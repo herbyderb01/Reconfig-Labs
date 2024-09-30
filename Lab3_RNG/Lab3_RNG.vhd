76dbcf0327baa628a13cf6988ff62aa50a7c4fb8
deaf-user <nate.walker.timp@gmail.com>
2024-09-29 18:57:35 -0600

    added basic code for top module and RNG

--- Lab3_RNG/Lab3_RNG.vhd
+++ Lab3_RNG/Lab3_RNG.vhd
@@ -2,152 +2,80 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Lab3_RNG is
-	port (
-		HEX0 : out std_logic_vector(7 downto 0);
-		HEX1 : out std_logic_vector(7 downto 0);
-		HEX2 : out std_logic_vector(7 downto 0);
-		HEX3 : out std_logic_vector(7 downto 0);
-		HEX4 : out std_logic_vector(7 downto 0);
-		HEX5 : out std_logic_vector(7 downto 0);
-		ADC_CLK_10 : IN STD_LOGIC;
-		MAX10_CLK1_50 : IN STD_LOGIC;
-		MAX10_CLK2_50 : IN STD_LOGIC;
-		KEY : IN STD_LOGIC_VECTOR(1 DOWNTO 0)
+entity lab2_RNG is
+	generic (
+		N : integer := 8
 	);
-end Lab3_RNG;
+	port (
+		HEX0 : out std_logic_vector(N - 1 downto 0);
+		HEX1 : out std_logic_vector(N - 1 downto 0);
+		HEX2 : out std_logic_vector(N - 1 downto 0);
+		HEX3 : out std_logic_vector(N - 1 downto 0);
+		HEX4 : out std_logic_vector(N - 1 downto 0);
+		HEX5 : out std_logic_vector(N - 1 downto 0);
+		KEY0 : in std_logic;
+		KEY1 : in std_logic;
+		ADC_clk_10 : in std_logic;
+		MAX10_CLK1_50 : in std_logic;
+		MAX10_CLK2_50 : in std_logic
+	);
+end lab2_RNG;
 
-
-architecture componentlist of Lab3_RNG is
-	component HEX_seven_seg_disp_6
-		port (
-			clk : in std_logic;
-			IN0 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			IN1 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			IN2 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			IN3 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			IN4 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			IN5 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-			HEX0 : out std_logic_vector(7 downto 0);
-			HEX1 : out std_logic_vector(7 downto 0);
-			HEX2 : out std_logic_vector(7 downto 0);
-			HEX3 : out std_logic_vector(7 downto 0);
-			HEX4 : out std_logic_vector(7 downto 0);
-			HEX5 : out std_logic_vector(7 downto 0)
-		);
-	end component HEX_seven_seg_disp_6;
-	
-	component counter
+architecture componentlist of lab2_RNG is
+	component RNG
 		generic (
-			n : integer := 4;
-			div : integer := 4;
-			div_min : integer := 9
+			N : integer := 8;
+			M : integer := 8
 		);
-		port (
+		port(
 			clk : in std_logic;
-			rst_l : in  std_logic;
-			go : in std_logic;
-			output : out std_logic_vector((n-1) downto 0)
+			en : in std_logic;
+			rst : in std_logic;
+			rn : out std_logic_vector ( N - 1 downto 0)
 		);
-	end component counter;
-	signal IN0 : std_logic_vector(3 downto 0);
-	signal IN1 : std_logic_vector(3 downto 0);
-	signal IN2 : std_logic_vector(3 downto 0);
-	signal IN3 : std_logic_vector(3 downto 0);
-	signal IN4 : std_logic_vector(3 downto 0);
-	signal IN5 : std_logic_vector(3 downto 0);
-	signal hex4_temp : std_logic_vector(7 downto 0);
-	signal hex2_temp : std_logic_vector(7 downto 0);
-begin
-	display : HEX_seven_seg_disp_6
-		port map (
-			clk => MAX10_CLK1_50,
-			IN0 => IN0,
-			IN1 => IN1,
-			IN2 => IN2,
-			IN3 => IN3,
-			IN4 => IN4,
-			IN5 => IN5,
-			HEX0 => HEX0,
-			HEX1 => HEX1,
-			HEX2 => HEX2_temp,
-			HEX3 => HEX3,
-			HEX4 => HEX4_temp,
-			HEX5 => HEX5
+	end component RNG;
+	
+	component HEX_seven_seg_disp
+		port (
+			hex : in std_logic_vector(3 downto 0);
+			clk : in std_logic;
+			oseg : out std_logic_vector(7 downto 0)
 		);
-	counter_H2 : counter
-		generic map (
-			n => 4,
-			div => 500000,
-			div_min => 9
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN0
-		);
-	counter_H1 : counter
-		generic map (
-			n => 4,
-			div =>5000000,
-			div_min => 9
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN1
-		);
-	counter_S2 : counter
-		generic map (
-			n => 4,
-			div => 50000000,
-			div_min => 9
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN2
-		);
-	counter_S1 : counter
-		generic map (
-			n => 4,
-			div => 500000000,
-			div_min => 5
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN3
-		);
-	counter_M2 : counter
-		generic map (
-			n => 4,
-			div => 3000000000,
-			div_min => 9
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN4
-		);
-	counter_M1 : counter
-		generic map (
-			n => 4,
-			div => 30000000000,
-			div_min => 5
-		)
-		port map (
-			clk => MAX10_CLK1_50,
-			rst_l => key(0),
-			go => key(1),
-			output => IN5
-		);
-	HEX4 <= "01111111" and HEX4_temp;
-	HEX2 <= "01111111" and HEX2_temp;
+	end component HEX_seven_seg_disp;
+	
+	signal rn : std_logic_vector (N - 1 downto 0);
+	
 
+begin
+
+	RNG1 : RNG 
+		generic map (
+			N => 8,
+			M => 8
+		);
+		port map (
+			clk => ADC_CLK_10,
+			en => KEY0,
+			rst => KEY1,
+			rn => rn
+		);
+		
+	disp1 : HEX_seven_seg_disp
+		port map (
+			hex => rn(3 downto 0),
+			clk => ADC_CLK_10,
+			oseg => HEX0
+		);
+	
+	disp2 : HEX_seven_seg_disp
+		port map (
+			hex => rn(7 downto 4),
+			clk => ADC_CLK_10,
+			oseg => HEX1
+		);
+	
+	HEX2 <= '11111111';
+	HEX3 <= '11111111';
+	HEX4 <= '11111111';
+	HEX5 <= '11111111';
 end componentlist;
